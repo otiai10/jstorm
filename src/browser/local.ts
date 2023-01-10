5742c948e78e91d8a76a09cd0edc9716f8d30420
Hiromu OCHIAI <otiai10@gmail.com>
2023-01-10 10:23:25 +0900

    Fix

--- src/browser/local.ts
+++ src/browser/local.ts
@@ -1,7 +1,8 @@
 import * as base from "..";
+import * as polyfill from "../polyfill";
 import { StorageArea } from "../interface";
 
 export class Model extends base.Model {
-    static override __area__: StorageArea = base.polyfill.use(globalThis.localStorage);
+    static override __area__: StorageArea = polyfill.use(globalThis.localStorage);
 }
 export { Types } from "../types";
--- src/browser/session.ts
+++ src/browser/session.ts
@@ -1,7 +1,8 @@
 import * as base from "..";
+import * as polyfill from "../polyfill";
 import { StorageArea } from "../interface";
 
 export class Model extends base.Model {
-    static override __area__: StorageArea = base.polyfill.use(globalThis.sessionStorage);
+    static override __area__: StorageArea = polyfill.use(globalThis.sessionStorage);
 }
 export { Types } from "../types";
